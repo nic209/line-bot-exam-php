adccbb8dcd96b5c2b655528e02195136305f87ac
nic209 <ninenic@gmail.com>
2018-04-22 12:27:39 +0700

    me

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '+i9RPw+AJNMl8xvrNnzZlAhC9TnSjzEEujUkt797K1TafJHV9W+9LR4Wohz7AkJnbDbRBBdYBOLrf24O0a7VnHCyTJe49jzbGI1lpXBoDPQlDhDI6IqvvLddpUtnabzCDA0MZRobVQSz2cz8KCu1zwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
